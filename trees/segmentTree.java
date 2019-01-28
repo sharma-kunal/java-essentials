11d1ef59f3e4a040a2a78aa0341e655988967f26
sharma-kunal <kunalsharma6914@gmail.com>
2019-01-29 00:47:32 +0530

    Updated Segment tree

--- trees/segmentTree.java
+++ trees/segmentTree.java
@@ -25,27 +25,18 @@ class segmentTree {
     System.out.println();
   }
 
-  public static void updatechild(int start, int end, int i) {
-    tree[i]+=1;
-    if(start == end) {
-      return;
-    }
-    int mid = (start+end)/2;
-    updatechild(start, mid, 2*i+1);
-    updatechild(mid+1, end, 2*i+2);
-  }
-
   public static int update(int start, int end, int a, int b, int i) {
-    if(a <= start && b >= end) {
-      //tree[i]+=1;
-      updatechild(start, end, i);
+    if(a > end || b < start){
+      return Integer.MAX_VALUE;
+    }
+    if(start == end && (start >= a || start <= b)) {
+      tree[i]+=1;
       return tree[i];
     }
-    if(a > end || b < start){
-      return 99999;
-    }
     int mid = (start+end)/2;
-    tree[i] = Math.min(update(start, mid, a, b, 2*i+1),update(mid+1, end, a, b, 2*i+2));
+    update(start, mid, a, b, 2*i+1);
+    update(mid+1, end, a, b, 2*i+2);
+    tree[i] = Math.min(tree[2*i+1], tree[2*i+2]);
     return tree[i];
   }
 }
@@ -56,7 +47,7 @@ class main {
     segmentTree s = new segmentTree(a.length);
     s.build(a, 0, a.length-1, 0);
     s.print();
-    s.update(0, 6, 2, 5, 0);
+    s.update(0, 6, 2, 4, 0);
     s.print();
   }
 }
